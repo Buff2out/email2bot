f7c359f4cf1ac7f5bdb115cabf8dd63e549e30f4
Rockython <61116928+Rockython@users.noreply.github.com>
2022-05-29 13:53:29 +0300

    Добавлены тестовые функции и тестовые запросы

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ TOKEN = "5259385883:AAFm-4DYkD8wEznwoSfyY9GLD9u5hdvrgg0"
 
 bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши 'Привет'")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+bot.polling(none_stop=True, interval=0)
