8f3f4b3d863f9227f28ec3880a96a9ddab3de9a1
Rockython <61116928+Rockython@users.noreply.github.com>
2022-06-09 19:38:26 +0300

    Добавлен поиск сообщений в коллекции

--- main.py
+++ main.py
@@ -55,7 +55,13 @@ def try_auth():
 def search_messages(service, query):
     result = service.users().messages().list(userId='me',q=query).execute()
     messages = [ ]
-    
+    if 'messages' in result:
+        messages.extend(result['messages'])
+    while 'nextPageToken' in result:
+        page_token = result['nextPageToken']
+        result = service.users().messages().list(userId='me',q=query, pageToken=page_token).execute()
+        if 'messages' in result:
+            messages.extend(result['messages'])
     return messages
 
 
