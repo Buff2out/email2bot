2cf900be66e2a205b1978692a0dd28036ee3b2c6
Rockython <61116928+Rockython@users.noreply.github.com>
2022-06-16 22:05:42 +0300

    Добавлено определение вспомогательной функции, работающей с форматом данных

--- main.py
+++ main.py
@@ -54,7 +54,11 @@ def try_auth():
 
 
 def get_size_format(b, factor=1024, suffix="B"):
-    pass
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if b < factor:
+            return f"{b:.2f}{unit}{suffix}"
+        b /= factor
+    return f"{b:.2f}Y{suffix}"
 
 
 
